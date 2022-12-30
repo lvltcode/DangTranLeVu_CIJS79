df37b8c0e15fe2442e93bcceb322ddb5b6bc40c5
Vu Le <lvltcode@gmail.com>
2022-12-30 18:07:44 +0700

    Add tailwind

--- session-3/src/TaskList.js
+++ session-3/src/TaskList.js
@@ -4,9 +4,9 @@ import './TaskList.css'
 
 const TaskList = (props) => {
     return (
-      <div className="taskList">
+      <div className="flex justify-between items-center mt-6">
         <form>
-        <input type="radio"/>
+        <input className='mr-4' type="radio"/>
         <label>{props.taskName}</label>
         </form>
         </div>
--- session-3/src/TodoList.js
+++ session-3/src/TodoList.js
@@ -2,24 +2,27 @@ import './TodoList.css'
 import TaskList from './TaskList';
 
 function TodoList() {
+  const tasks = ["Clean up bedroom", "Buy some milk", "Jogging", "Learn React", "Doing Exercise"];
     return (
       <div className="TodoList">
         <div className='row'>
         <form>
-            <input className='inputTask' placeholder="Enter your task here" />
+            <input className='inputTask border-b border-b-black outline-none py-1 px-3 text-xl w-full mb-4' placeholder="Enter your task here" />
             
           </form>
-          <TaskList taskName='Clean up the bedroom'/>
+          {tasks.map(item => {
+        return <TaskList key={item} taskName={item} />
+      })}
+          {/* <TaskList taskName='Clean up the bedroom'/>
           <TaskList taskName='Buy milk'/>
           <TaskList taskName='Jogging'/>
           <TaskList taskName='Learn React'/>
-          <TaskList taskName='Doing Exercise'/>
+          <TaskList taskName='Doing Exercise'/> */}
         </div>
-        <div className='row'>
-        5 tasks left!
-        MindX todolist
-        
-        </div>
+        <div className="flex justify-between items-center mt-6">
+        <div>{tasks.length} tasks left!</div>
+        <div>MindX todolist</div>
+      </div>
         </div>
     );
   }
